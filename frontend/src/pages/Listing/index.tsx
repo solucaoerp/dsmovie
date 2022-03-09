0484d94d91cd457d6e6ecf70079bff3c59a792ce
Charles Borges <solucao.erp@gmail.com>
2022-03-09 18:55:19 -0300

    definition and organization of stars

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,12 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 export default function Listing() {
     return (
+        <>
         <Pagination />
+
+        <MovieStars />
+        </>
     );
 }
