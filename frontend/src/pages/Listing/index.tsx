7b87dc954329adb84fedf0472d13ed4056d3c600
Charles Borges <solucao.erp@gmail.com>
2022-03-09 19:35:06 -0300

    addition of the MovieCard component

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,45 @@
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 export default function Listing() {
     return (
         <>
-        <Pagination />
+            <Pagination />
 
-        <MovieScore />
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
+
         </>
     );
 }
