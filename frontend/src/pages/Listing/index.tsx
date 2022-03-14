dc8e52607e0773552da7bae2be1dd17bcc2e36c9
Charles Borges <solucao.erp@gmail.com>
2022-03-14 12:37:22 -0300

    frontend: controlled execution of the request - useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,23 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 export default function Listing() {
 
-    // FORMA ERRADA DE REALIZAR A REQUISIÇÃO
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    // CORRECT! controlled execution of the request
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(response.data); // checkdebug on browser
+                setPageNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
